ee83b72886c3671f8e6c20ddab20984b8ee3cf1a
Foolson <johan.olsson.91@gmail.com>
2016-03-14 12:21:59 +0100

    stuff and other

---
+++ 42.ps1
@@ -0,0 +1,11 @@
+﻿################## METADATA ##################
+# NAME: your full name
+# USERNAME: your login name
+# COURSE: this course’s name
+# ASSIGNMENT: name and number of assignment
+# DATE OF LAST CHANGE: date in ISO~8601
+##############################################
+
+Set-StrictMode -Version Latest
+
+Import-Module ActiveDirectory
