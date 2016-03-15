c81921e74e95a9ac5598cbe003e7c869c9ffef96
Foolson <johan.olsson.91@gmail.com>
2016-03-15 14:51:24 +0100

    Added strict mode

--- 61.ps1
+++ 61.ps1
@@ -5,6 +5,8 @@
 # ASSIGNMENT: Powershell 6.1
 # DATE OF LAST CHANGE: 2016-03-15
 ##############################################
+Set-StrictMode -Version Latest
+
 $File = Read-Host "Full Path To File"
 $array = @()
 ForEach($Computer In (Get-ADComputer -Filter *).Name){
