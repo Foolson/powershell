b355b585d287f05b3eb2ce4ee1ac7db1ec4258d7
Johan Olsson <johan.olsson.91@gmail.com>
2016-04-03 13:27:24 +0200

    Comments done

--- 42.ps1
+++ 42.ps1
@@ -10,11 +10,17 @@ Set-StrictMode -Version Latest
 
 Import-Module ActiveDirectory
 
+# Loop through all relevant OU in the domain
 ForEach($OU In (Get-ADOrganizationalUnit -SearchBase 'OU=Accounts,DC=scripting,DC=nsa,DC=his,DC=se' -Filter *)){
+    # Specify the shadow group name
     $OUSG = "SG_" + $OU.Name
+    # Check if the shadow group exist
     $ShadowGroup = (Get-ADGroup -Filter {Name -Like $OUSG})
     If($ShadowGroup -ne $null){
+        # Get the relevant shadow group name
         $ShadowGroupName = $ShadowGroup.Name
+        # Loop through each user in the relevant shadow group
+        # If the user does not exist in the relevant OU then remove the user from the shadow group
         ForEach($User In (Get-ADGroupMember $ShadowGroup)){
             $UserName = $User.Name
              If(-Not(Get-ADUser -SearchBase $OU.DistinguishedName -Filter {Name -Like $UserName})){
@@ -22,6 +28,8 @@ ForEach($OU In (Get-ADOrganizationalUnit -SearchBase 'OU=Accounts,DC=scripting,D
                 Echo "$UserName Removed from $ShadowGroupName"
              }
         }
+        # Loop through each user in the relevant OU
+        # If the user does not exist in the relevant shadow group then add the user to the shadow group
         ForEach($User In (Get-ADUser -SearchBase $OU.DistinguishedName -Filter *)){
             $UserName = $User.Name
             If(-Not(Get-ADGroupMember -Identity $ShadowGroup | Where {$_.Name -eq $UserName})){
